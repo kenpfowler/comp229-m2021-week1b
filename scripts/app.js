be92373c49523d2c127ee1ca47bdfd9c712ab19f
Kenny <kenpfowler@gmail.com>
2021-05-12 12:02:38 -0400

    added IIFE with definition and GitHub link

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,8 @@
 "use strict";
 
-const myVar = 10;
-console.log(`My number is : ${myVar}`);
+const myGitHub = "https://github.com/kenpfowler/comp229-m2021-week1b";
+(function () {
+  document.write(`  <p>This line is displayed by an IIFE (Immediately Invoked Function Expression)</p>
+                    <p>An IIFE is a Javascript Function that runs as soon as it's defined</p>
+                    <p>Find the code at my <a target="_blank" href="${myGitHub}">GitHub repo</a></p>`);
+})();
